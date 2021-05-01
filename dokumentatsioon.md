c82832eb34f7519006a13fdaf6107ac43192b7ff
Henri Sellis <henri.sellis@gmail.com>
2021-05-01 20:45:57 +0300

    Implemented day length graph using google-vue-charts

--- dokumentatsioon.md
+++ dokumentatsioon.md
@@ -4,14 +4,17 @@
 0,5h - projekti üles seadmine ning OpenLayers ja Leafleti kohta lugemine
 
 28.04:
-1h - päikesetõusu ja -loojangu aja arvutamise kohta info otsimine
-1h - Etapp 1 implementeerimine
+1h -   päikesetõusu ja -loojangu aja arvutamise kohta info otsimine
+1h -   Etapp 1 implementeerimine
 
 29.04:
-1h - SunCalc libraryle üleminek ja veebilehe kujundamine
-3h - Vue Leafletiga kaardi tööle saamine ja kaardilt info kätte saamine
+1h -   SunCalc libraryle üleminek ja veebilehe kujundamine
+3h -   Vue Leafletiga kaardi tööle saamine ja kaardilt info kätte saamine
 0.5h - kujunduse parandamine
 
 30.04:
 1.5h - manuaalselt koordinaatide valimine kajastub automaatselt ka kaardil,
        l-markeri bugfix
+
+01.05:
+2h -   erinevate graafikuraamistike uurimine, Google Vue Chartsi valimine ja graafiku implementeerimine
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -8,17 +8,42 @@
       <button @click="calculateTimeInfo">Calculate</button>
     </div>
     <div id="results-container">
-      <label>Sunrise: {{sunRise}}</label>
+      <label>Sunrise: {{ sunRise }}</label>
       <br>
-      <label>Sunset: {{sunSet}}</label>
+      <label>Sunset: {{ sunSet }}</label>
       <br>
-      <label>Length of day: {{dayLength}}</label>
+      <label>Length of day: {{ dayLength }}</label>
       <br>
     </div>
+    <section id="main-graph-section">
+      <h2>Graph visualizing</h2>
+      <h4>To visualize the length of day over a period of time, select the time period</h4>
+      <div id="time-period-selecting">
+        <div>
+          <label>Start date: </label>
+          <input type="datetime-local" v-model="startDate">
+        </div>
+        <div>
+          <label>End date: </label>
+          <input type="datetime-local" v-model="endDate">
+        </div>
+      </div>
+
+      <div id="graph-area" v-if="startDate && endDate">
+        <GChart
+            type="AreaChart"
+            :data="chartData"
+            :options="chartOptions"
+        />
+      </div>
+
+    </section>
   </section>
 </template>
 
 <script>
+import {GChart} from 'vue-google-charts'
+
 const SunCalc = require('suncalc');
 
 export default {
@@ -30,7 +55,36 @@ export default {
       sunRise: null,
       sunSet: null,
       dayLength: null,
+
+      startDate: null,
+      endDate: null,
+
+      chartOptions: {
+        title: 'Day length visualization',
+        hAxis: {title: 'Date',  titleTextStyle: {color: '#333'}},
+        vAxis: {title: 'Day length in hours'}
+      }
     }
+  },
+  computed: {
+    chartData: function () {
+      let diffInDays = (new Date(this.endDate) - new Date(this.startDate)) / 1000 / 60 / 60 / 24;
+      console.log("diff: " + diffInDays)
+      let data = [
+        //['Date', 'Length of day']
+      ]
+      for (let i = 0; i < diffInDays; i++) {
+        let date = new Date(this.startDate)
+        date.setDate(date.getDate() + i)
+        let times = SunCalc.getTimes(date, this.selectedCoords.lat, this.selectedCoords.lng)
+        let dayLength = (times.sunset - times.sunrise) / 1000 / 60 / 60
+        data.push([date, dayLength])
+      }
+      data.unshift(['Date', 'Length of day'])
+      console.log(data)
+      return data
+    }
+
   },
   props: {
     selectedCoords: Object,
@@ -57,10 +111,18 @@ export default {
       this.sunSet = sunsetStr
       this.dayLength = dayLengthTime
     },
+  },
+  components: {
+    GChart
   }
 }
 </script>
 
 <style scoped>
+
+#graph-area {
+  height: 600px;
+  width: 100%;
+}
 
 </style>
